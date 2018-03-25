02daaeec8fab36fbb12000ae27a0edeb6372296a
nkoseki <koseki.nobuyuki@gmail.com>
2018-03-26 08:28:30 +0900

    JSPとServletの連携

---
+++ src/main/java/sample/HelloWorld.java
@@ -0,0 +1,27 @@
+package sample;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		request.setCharacterEncoding("utf-8");
+		// formから値を取得
+	    String val1 = request.getParameter("param1");
+	    
+		//response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.print("HelloWorld: ");
+		out.println(val1);
+		out.println("</html>");
+		out.close();
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
-<%@ page contentType="text/html; charset=utf-8" %>
+<%@ page contentType="text/html; charset=utf-8"
+								pageEncoding="UTF-8" %>
 <html lang="ja">
 <head>
 <meta charset="utf-8">
@@ -7,5 +8,13 @@
 <body>
 <h2>Hello World!</h2>
 試験試験
+
+    <%= request.getAttribute("foo") %>
+
+    <form method="post" action="servlet/hello">
+        何か入力して: <input type="text" name="param1">
+        <button type="submit">送信</button>
+    </form>
+
 </body>
 </html>
