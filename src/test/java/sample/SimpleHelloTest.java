598d7703578dfb9991a1069ebb81e412e7c5cbc5
nkoseki <koseki.nobuyuki@gmail.com>
2018-03-26 12:56:28 +0900

    Jenkinsのトリガを設定

--- src/test/java/sample/SimpleHelloTest.java
+++ src/test/java/sample/SimpleHelloTest.java
@@ -9,7 +9,7 @@ public class SimpleHelloTest extends TestCase {
 	@Test
 	public void testFirst(){
 		sample.SimpleHello hello = new sample.SimpleHello();
-		hello.setId(9);
+		hello.setId(10);
 		assertEquals(9, hello.getId());
 		
 	}
