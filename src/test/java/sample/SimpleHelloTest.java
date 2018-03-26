f0c9867c454f784780fb8479160e876bca52d4cb
nkoseki <koseki.nobuyuki@gmail.com>
2018-03-26 12:50:22 +0900

    GitHubにhookを仕掛ける→Pushしたら、自動でjenkinsがビルド

--- src/test/java/sample/SimpleHelloTest.java
+++ src/test/java/sample/SimpleHelloTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestCase;
 public class SimpleHelloTest extends TestCase {
 
 	@Test
-	public void testfirst(){
+	public void testFirst(){
 		sample.SimpleHello hello = new sample.SimpleHello();
 		hello.setId(9);
 		assertEquals(9, hello.getId());
